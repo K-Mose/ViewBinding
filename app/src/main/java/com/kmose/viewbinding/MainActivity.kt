43cf02debf50a2aa28e7d388cfb6122076d8bd94
user <km_413@naver.com>
2022-04-22 14:32:51 +0900

    Project Setup

--- app/src/main/java/com/kmose/viewbinding/MainActivity.kt
+++ app/src/main/java/com/kmose/viewbinding/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.kmose.viewbinding
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import android.os.Bundle
+import com.kmose.viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(R.layout.activity_main)
+
+        binding.btnSubmit.setOnClickListener {
+            displayGreeting()
+        }
+    }
+
+    private fun displayGreeting() {
+
+        binding.greetingTextView.text = "Hello! " + binding.etNameText.text
     }
 }
